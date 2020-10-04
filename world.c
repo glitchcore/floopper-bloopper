5cc9a6c6f260990a6541613844e4a22401eb76bd
aanper <mail@s3f.ru>
2020-10-04 21:28:55 +0300

    fix combo label

--- world.c
+++ world.c
@@ -38,9 +38,9 @@ void render_world(GameState* state, u8g2_t* fb, uint32_t t) {
 
     if(state->combo_text) {
         u8g2_SetDrawColor(fb, 0);
-        u8g2_DrawBox(fb, SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, 25);
+        u8g2_DrawBox(fb, SCREEN_WIDTH / 2 - 15, 0, SCREEN_WIDTH / 2 + 15, 25);
         u8g2_SetDrawColor(fb, 1);
-        u8g2_DrawFrame(fb, SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, 25);
-        u8g2_DrawStr(fb, SCREEN_WIDTH / 2 + 10, 20, "COCOCOCOMBO!");
+        u8g2_DrawFrame(fb, SCREEN_WIDTH / 2 - 15, 0, SCREEN_WIDTH / 2 + 15, 25);
+        u8g2_DrawStr(fb, SCREEN_WIDTH / 2 - 10, 15, "COCOCOCOMBO!");
     }
 }
