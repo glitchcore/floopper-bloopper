77c230c8ad4435d26fb5370f8a0bcabadfaf0b46
glitchcore <mail@s3f.ru>
2020-10-04 17:55:52 +0600

    floor fix

--- floopper-bloopper.c
+++ floopper-bloopper.c
@@ -218,6 +218,15 @@ void update_player_coordinates(GameState* state, uint32_t dt) {
     int32_t floor_height = SCREEN_HEIGHT * SCALE -
         HEIGHT_MAP[(abs(state->player_global.x) / SCALE) % WORLD_WIDTH] -
         PLAYER_HEIGHT * SCALE;
+    int32_t f_h;
+    for(size_t i = 1; i < PLAYER_WIDTH; i++) {
+        f_h = SCREEN_HEIGHT * SCALE -
+            HEIGHT_MAP[(abs(state->player_global.x + i) / SCALE) % WORLD_WIDTH] -
+            PLAYER_HEIGHT * SCALE;
+        if (f_h > floor_height) {
+            floor_height = f_h;
+        }
+    }
 
     if(state->player_global.y >= floor_height){
         state->player.y = floor_height;
